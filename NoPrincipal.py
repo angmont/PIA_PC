4e86e91cba515240e7fc0f9c11c6fe7e3b753a2a
angmont <angela.montoyaldp@uanl.edu.mx>
2021-10-28 22:07:51 -0500

    Modificaciones
    Se agregó la tarea de cifrar al script

---
+++ NoPrincipal.py
@@ -0,0 +1,89 @@
+import requests
+
+def cifrar_mensaje(mensaje, key):
+  universo = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
+
+  translated = ''
+
+  for symbol in mensaje:
+    # Note: Only symbols in the `SYMBOLS` string can be encrypted/decrypted.
+    if symbol in universo:
+        symbolIndex = universo.find(symbol)
+        translatedIndex = symbolIndex + key
+        
+        if translatedIndex >= len(universo):
+            translatedIndex = translatedIndex - len(universo)
+        elif translatedIndex < 0:
+            translatedIndex = translatedIndex + len(universo)
+
+        translated = translated + universo[translatedIndex]
+    else:
+        # Append the symbol without encrypting/decrypting:
+        translated = translated + symbol
+
+  return(translated)
+
+
+def descifrar_mensaje(mensaje, key):
+ universo = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?.'
+
+ translated = ''
+
+
+ for symbol in mensaje:
+    # Note: Only symbols in the `SYMBOLS` string can be encrypted/decrypted.
+    if symbol in universo:
+        symbolIndex = universo.find(symbol)
+        translatedIndex = symbolIndex - key
+        
+        if translatedIndex >= len(universo):
+            translatedIndex = translatedIndex - len(universo)
+        elif translatedIndex < 0:
+            translatedIndex = translatedIndex + len(universo)
+
+        translated = translated + universo[translatedIndex]
+    else:
+        # Append the symbol without encrypting/decrypting:
+        translated = translated + symbol
+
+ return(translated)
+
+def cifrar_github(usuario, clave):
+  i = 0
+  try:
+    urlrepos = "https://api.github.com/users/" + usuario + "/repos"
+    resp1 = requests.get(urlrepos)
+    dic1 = (resp1.json())
+    if len(dic1) == 0:
+      return None
+    try:
+      if dic1['message'] == 'Not Found':
+        print (dic1['message'])
+        return (dic1['message'])
+    except:
+      pass
+    escritura = open("encriptado.txt", "a")
+    for element in dic1:
+      i = i + 1
+      adios =("Repositorio " + str(i) + "\n\n")
+      titulo = cifrar_mensaje(adios, clave)
+      escritura.write(titulo)
+      for key in element:
+        hello = (str(key) + " = " + str(element[key]) + "\n")
+        msj2 = cifrar_mensaje(hello, clave)
+        escritura.write(msj2)
+      escritura.write("\n\n")
+    escritura.close()
+  except Exception as e:
+    print(e)
+
+def descifrar_github(path, clave):
+
+  lectura = open(path, "r")
+  escritura = open("descifrado.txt", "a")
+  for linea in lectura:
+    mensaje = descifrar_mensaje(linea, clave)
+    escritura.write(mensaje)
+  print("Hecho!")
+  escritura.close()
+  lectura.close()
--- Principal.py
+++ Principal.py
@@ -1,1 +1,43 @@
-print("Hola")
+import NoPrincipal
+import argparse
+
+if __name__ == "__main__":
+  description= ("Este script realiza una gran diversa cantidad de tareas " +
+  "las cuales son las siguientes: realizar cifrados, obtener metadata, " +
+  "escaneo de puertos, envio de correos y ")
+  parser = argparse.ArgumentParser(description="PIA", epilog=description, formatter_class=argparse.RawDescriptionHelpFormatter)
+  parser.add_argument("-tarea", metavar='TAREA', dest="tarea", choices=['cifrar'] , help='Se elige la tarea a realizar', required=True)
+  parser.add_argument("-modo", metavar='MODO', dest="modo", choices=['cifmensaje', 'desmensaje', 'cifgithub', 'desgithub'] , help='Si desea utilizar la tarea de cifrado/descifrado, es necesario especificiar el modo')
+  parser.add_argument("-mensaje", metavar='MENSAJE', dest="mensaje", type=str, help='Se debe poner un mensaje el cual se quiera cifrar o descifrar.')
+  parser.add_argument("-llave", metavar='LLAVE', dest="llave", type=int, help='Se utiliza para saber a base de cual llave se cifra o descifra el mensaje')
+  parser.add_argument("-usuario", metavar='USUARIO', dest="usuario", type=str, help='Es un argumento necesario para la funcion de cifrar los resultados obtenidos de la API de Github')
+  parser.add_argument("-ruta", metavar='RUTA', dest="ruta", type=str, help='Ruta necesaria para el txt que se va a descifrar')
+  params = parser.parse_args()
+try: 
+    tarea = (params.tarea)
+except Exception as e:
+    print(e)
+    exit
+
+if tarea == 'cifrar':
+    try:
+        modo = (params.modo)
+        llave = (params.llave)
+        if (modo == 'cifmensaje') or (modo == 'desmensaje'):
+            mensaje = (params.mensaje)
+            if modo == 'cifmensaje':
+                print(NoPrincipal.cifrar_mensaje(mensaje, llave))
+            else:
+                print(NoPrincipal.descifrar_mensaje(mensaje, llave))
+
+        elif modo == 'cifgithub':
+            print('hola')
+            usuario = (params.usuario)
+            NoPrincipal.cifrar_github(usuario, llave)
+        else:
+            ruta = (params.ruta)
+            NoPrincipal.descifrar_github(ruta, llave)
+    except Exception as e:
+        print(e)
+        exit
+
