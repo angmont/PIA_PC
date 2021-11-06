a312383ca7c5d47eacab8b566f6f42530713e643
angmont <angela.montoyaldp@uanl.edu.mx>
2021-11-06 10:32:39 -0600

    Update Principal.py
    Se agregó lo referente al llamado del script de powershell

--- Principal.py
+++ Principal.py
@@ -1,13 +1,15 @@
+import subprocess
 import NoPrincipal
 import Correos_Modulo
 import argparse
+import os, time
 
 if __name__ == "__main__":
   description= ("Este script realiza una gran diversa cantidad de tareas " +
   "las cuales son las siguientes: realizar cifrados, obtener metadata, " +
   "escaneo de puertos, envio de correos y ")
   parser = argparse.ArgumentParser(description="PIA", epilog=description, formatter_class=argparse.RawDescriptionHelpFormatter)
-  parser.add_argument("-tarea", metavar='TAREA', dest="tarea", choices=['cifrar','correos'] , help='Se elige la tarea a realizar', required=True)
+  parser.add_argument("-tarea", metavar='TAREA', dest="tarea", choices=['cifrar','correos', 'dns'] , help='Se elige la tarea a realizar', required=True)
   parser.add_argument("-modo", metavar='MODO', dest="modo", choices=['cifmensaje', 'desmensaje', 'cifgithub', 'desgithub'] , help='Si desea utilizar la tarea de cifrado/descifrado, es necesario especificiar el modo')
   parser.add_argument("-mensaje", metavar='MENSAJE', dest="mensaje", type=str, help='Se debe poner un mensaje el cual se quiera cifrar o descifrar.')
   parser.add_argument("-llave", metavar='LLAVE', dest="llave", type=int, help='Se utiliza para saber a base de cual llave se cifra o descifra el mensaje')
@@ -46,7 +48,7 @@ if tarea == 'cifrar':
     except Exception as e:
         print(e)
         exit
-if tarea == 'correos':
+elif tarea == 'correos':
     try:
         Remitente = (params.remitente)
         Destinatario = (params.destinatario)
@@ -65,4 +67,12 @@ if tarea == 'correos':
      except Exception as e:
         print(e)
         exit
-        
+elif tarea == 'dns':  
+    try:
+	    print()
+	    script_p = subprocess.Popen([r'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe',
+                            '-ExecutionPolicy', 'Unrestricted', './DNS.ps1'], cwd=os.getcwd())
+    
+	    script_p.wait()
+    except Exception as e:
+	    print(e)
