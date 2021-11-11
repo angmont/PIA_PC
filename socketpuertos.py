68b70ce545a38ab703ed5d041fe35f9a7ef1dce4
ianisra <90713096+ianisra@users.noreply.github.com>
2021-11-11 13:22:49 -0600

    Update socketpuertos.py

--- socketpuertos.py
+++ socketpuertos.py
@@ -1,9 +1,18 @@
 #Socket para revisar puertos 
 #Ayuda, se me habia olvidado el github JAJSJA SOS
 
-#import Socket
+import Socket
 
-def checoPuertos(dominio)
-    resultado = socket.gethostbyname_ex(dominio)
-    
-    print("el host es" + resultado)
+def checoPuertos(ip, portlist):
+    try:
+        for port in portlist:
+            sock = socket.socket(socket.AF_NET,socket.SOCK_STREAM)
+            sock.settimeout(7)
+            result = sock.connect_ex((ip,port))
+            if result == 0:
+                print("Puerto {}: \t Abierto".format(port))
+            else
+                print("Puerto {}: \t Cerrado".format(port))
+                
+     except:
+        pass
