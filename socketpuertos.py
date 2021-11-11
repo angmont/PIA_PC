741c1f331e4d5d25f2fa54b95fd19d5ae2c28574
ianisra <90713096+ianisra@users.noreply.github.com>
2021-11-11 17:11:06 -0600

    Update socketpuertos.py

--- socketpuertos.py
+++ socketpuertos.py
@@ -1,16 +1,14 @@
 import socket
 import sys
 
-def checoPuertos(ip, portlist):
-    try:
-        for port in portlist:
-            sock = socket.socket(socket.AF_NET,socket.SOCK_STREAM)
-            sock.settimeout(7)
-            result = sock.connect_ex((ip,port))
-            if result == 0:
-                print("Puerto {}: \t Abierto".format(port))
-            else
-                print("Puerto {}: \t Cerrado".format(port))
-                
-     except:
-        pass
+def checoPuertos(ip, portlist, puertoss):
+  print("Empieza el escaneo de la ip: " + ip + " Con los puertos: " + puertoss)
+  for port in portlist:
+    sock = socket.socket(socket.AF_NET,socket.SOCK_STREAM)
+    sock.settimeout(7)
+    result = sock.connect_ex((ip,port))
+    if result == 0:
+      print("Puerto {}: \t Abierto".format(port))
+    else:
+      print("Puerto {}: \t Cerrado".format(port))
+  
