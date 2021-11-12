f4aa3f0c3c3f6af298bf7f5e7c771c0a7f6dd405
angmont <angela.montoyaldp@uanl.edu.mx>
2021-11-11 18:56:28 -0600

    Modificaciones

--- Principal.py
+++ Principal.py
@@ -3,6 +3,7 @@ import NoPrincipal
 import Correos_Modulo
 import socketpuertos
 import MetaDataPIA
+import webscraping
 import argparse
 import os, time
 
@@ -12,21 +13,23 @@ if __name__ == "__main__":
   "las cuales son las siguientes: realizar cifrados, obtener metadata, " +
   "escaneo de puertos, envio de correos y ")
   parser = argparse.ArgumentParser(description="PIA", epilog=description, formatter_class=argparse.RawDescriptionHelpFormatter)
-  parser.add_argument("-t", metavar='TAREA', dest="tarea", choices=['cifrar','correos', 'dns', 'puertos', 'metadata'] , help='Se elige la tarea a realizar', required=True)
-  parser.add_argument("-m", metavar='MODO', dest="modo", choices=['cifmensaje', 'desmensaje', 'cifgithub', 'desgithub'] , help='Si desea utilizar la tarea de cifrado/descifrado, es necesario especificiar el modo')
+  parser.add_argument("-t", metavar='TAREA', dest="tarea", choices=['cifrar','correos', 'dns', 'puertos', 'metadata', 'web'] , help='Se elige la tarea a realizar', required=True)
+  parser.add_argument("-m", metavar='MODO', dest="modo", 
+  choices=['cifmensaje', 'desmensaje', 'cifgithub', 'desgithub', 'busqueda', 'emails', 'pdf', 'img'] , help='Si desea utilizar la tarea de cifrado/descifrado, es necesario especificiar el modo')
   parser.add_argument("-msj", metavar='MENSAJE', dest="mensaje", type=str, help='Se debe poner un mensaje el cual se quiera cifrar o descifrar.')
   parser.add_argument("-key", metavar='LLAVE', dest="llave", type=int, help='Se utiliza para saber a base de cual llave se cifra o descifra el mensaje')
   parser.add_argument("-user", metavar='USUARIO', dest="usuario", type=str, help='Es un argumento necesario para la funcion de cifrar los resultados obtenidos de la API de Github')
   parser.add_argument("-ru", metavar='RUTA', dest="ruta", type=str, help='Ruta necesaria para el txt que se va a descifrar o donde se encuentran los arctivos pata la funcion de metadata')
   parser.add_argument("-rem", metavar='REMITENTE', dest="remitente", type=str, help='Correo del que se enviará el mensaje.')
   parser.add_argument("-des", metavar='DESTINATARIO', dest="destinatario", type=str, help='Correo que recibirá el mensaje.')
-  parser.add_argument("-dom", metavar= 'DOMINIO', dest="dominio", type=str, help='Dominio a investigar.')
+  parser.add_argument("-url", metavar= 'URL', dest="dominio", type=str, help='Url a investigar.')
   parser.add_argument("-cont", metavar='CONTENIDO', dest="contenido", type=str, help='Se debe poner un mensaje el cual se quiera enviar.', default="Hola mundo mundial")
   parser.add_argument("-asu", metavar='ASUNTO', dest="asunto", type=str, help='Se utiliza para poner el titulo que tendrá el correo.', default="Hola!")
   parser.add_argument("-ip", metavar='IP', dest="ip", type=str, help='Se debe introducir la ip a consultar, solo el ID de red.', default="172.217.15.")
   parser.add_argument("-ports", metavar='PUERTOS', dest="puertos", help='Introduce los puertos a revisar separados por una coma [80,800]', default= "80,800")
   parser.add_argument("-a", metavar='ARCHIVO', dest="archivo", choices=['imagen', 'imagenes', 'pdf', 'pdfs', 'word', 'words', 'mp3', 'mp3s'] , help='Si desea utilizar la tarea de sacar la metadata, es necesario especificiar el tipo de archivo')
   parser.add_argument("-mp", metavar= 'METAPATH', dest="metapath", type=str, help='Ruta donde se guardarán los metadatas encontrados.')
+  parser.add_argument("-bus", metavar='BUSQUEDA', dest="busqueda", type=str, help='Busqueda para realizar en google')
   params = parser.parse_args()
 try: 
     tarea = (params.tarea)
@@ -34,8 +37,9 @@ except Exception as e:
     print(e)
     exit
 
-if tarea == 'cifrar':
-    try:
+try:
+    if tarea == 'cifrar':
+
         modo = (params.modo)
         llave = (params.llave)
         if (modo == 'cifmensaje') or (modo == 'desmensaje'):
@@ -48,14 +52,14 @@ if tarea == 'cifrar':
         elif modo == 'cifgithub':
             usuario = (params.usuario)
             NoPrincipal.cifrar_github(usuario, llave)
-        else:
+        elif modo == 'desgithub':
             ruta = (params.ruta)
             NoPrincipal.descifrar_github(ruta, llave)
-    except Exception as e:
-        print(e)
-        exit
-elif tarea == 'correos':
-    try:
+        else:
+            print('Opción no válida para cifrado')
+
+    elif tarea == 'correos':
+
         remitente = (params.remitente)
         destinatario = (params.destinatario)
         mensaje = (params.contenido)
@@ -68,20 +72,17 @@ elif tarea == 'correos':
             exit()
         else:
             Correos_Modulo.GuardarInformacion(datos_encontrados, orga, remitente, destinatario, asunto, mensaje)
-    except Exception as e:
-        print(e)
-        exit
-elif tarea == 'dns':  
-    try:
+
+    elif tarea == 'dns':  
+
 	    print()
 	    script_p = subprocess.Popen([r'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe',
                             '-ExecutionPolicy', 'Unrestricted', './DNS.ps1'], cwd=os.getcwd())
     
 	    script_p.wait()
-    except Exception as e:
-	    print(e)
-elif tarea == 'metadata':
-    try:
+
+    elif tarea == 'metadata':
+
         archivo = (params.archivo)
         if (archivo == 'imagen') or (archivo == 'imagenes'):
             ruta = (params.ruta)
@@ -111,11 +112,9 @@ elif tarea == 'metadata':
                 MetaDataPIA.printOneMetaMp3(ruta, metapath)
             else:
                 MetaDataPIA.printAllMetaMp3(ruta, metapath)
-    except Exception as e:
-        print(e)
-        exit
-elif tarea == 'puertos':	
-    try:
+
+    elif tarea == 'puertos':	
+
       ip = params.ip
       print("Se revisará la ip: " + ip)
 	
@@ -127,6 +126,23 @@ elif tarea == 'puertos':
 		
       socketpuertos.checoPuertos(ip, portlist, puertoss)
 
-    except Exception as e:
-      print(e)
-      exit
+
+    elif tarea == 'web':
+
+        modo = params.modo
+        if modo == 'emails' or modo == 'pdf' or modo == 'img':
+            url = params.dominio
+            if modo == 'emails':
+                webscraping.find_emails(url)
+            elif modo == 'pdf':
+                webscraping.descargar_pdfs(url)
+            else:
+                webscraping.download_images(url)
+        elif modo == 'busqueda':
+            busqueda == params.busqueda
+            webscraping.busqueda_google(busqueda)
+        else:
+            print('Opcion no válida para web scraping')
+except Exception as e:
+    print(e)
+    exit
