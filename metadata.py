edf3098f1f60286728a5edad4d4b61660f1821e0
ianisra <90713096+ianisra@users.noreply.github.com>
2021-11-12 17:41:19 -0600

    Update metadata.py

--- metadata.py
+++ metadata.py
@@ -37,7 +37,7 @@ def printAllMetaImg(path, MetaPath):
         		print ("[+] Metadata for file: %s " %(name))
         		try:
                                 logging.info("Entramos a la funcion get_exif_metadata con un valor de: " + name)
-        			exif = get_exif_metadata(name)
+                                exif = get_exif_metadata(name)
                                 logging.info("Escribimos la metadata")
         			for metadata in exif:
         				fo.write("Metadata: %s - Value: %s " %(metadata, exif[metadata]) + "\n")
