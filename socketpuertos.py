3b503e233e989d21d25a0e56b960ea0ce2e350a9
ianisra <90713096+ianisra@users.noreply.github.com>
2021-11-11 18:03:41 -0600

    Update socketpuertos.py

--- socketpuertos.py
+++ socketpuertos.py
@@ -4,7 +4,7 @@ import sys
 def checoPuertos(ip, portlist, puertoss):
   print("Empieza el escaneo de la ip: " + ip + " Con los puertos: " + puertoss)
   for port in portlist:
-    sock = socket.socket(socket.AF_NET,socket.SOCK_STREAM)
+    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.settimeout(7)
     result = sock.connect_ex((ip,port))
     if result == 0:
