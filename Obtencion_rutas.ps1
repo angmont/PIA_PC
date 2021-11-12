5974cfc8cda9c1f88f2901058eda038bc15938c2
angmont <angela.montoyaldp@uanl.edu.mx>
2021-11-12 09:42:27 -0600

    Update Obtencion_rutas.ps1

--- Obtencion_rutas.ps1
+++ Obtencion_rutas.ps1
@@ -1,12 +1,31 @@
-﻿$ruta = Get-Location
-Write-Host $ruta
+﻿$ruta = Read-Host -prompt "Inserte la ruta a analizar`n"
 Write-Host ' '
-$archivos = Get-ChildItem $ruta -Recurse | Where-Object {$_.PSIsContainer -eq $false} | Select-Object Fullname
-$rutas = $archivos.Fullname
 
+try{
+    $archivos = Get-ChildItem $ruta -Recurse | Where-Object {$_.PSIsContainer -eq $false} | Select-Object Fullname
+    $rutas = $archivos.Fullname
+}catch{
+    Write-Host 'Ruta errónea'
+    exit
+}
+$op = Read-Host -prompt "¿Qué hash desea realizar?`n[1] Md5`n[2] Sha512`n[3] Sha256`n`n"
+switch($op){
+    1{ 
+        $num = 'md5'
+    }2{
+        $num = 'sha512'
+        }
+    }3{
+        $num = 'sha256'
+
+    }default{
+        Write-Host "Ingreso un valor inválido"
+        exit
+    }
+}
 foreach ($file in $rutas){
     $name = Split-Path $file -leaf
-    python Hashes.py $file $name
+    python Hashes.py $file $name $num
     #Write-Host $file
     }
 
