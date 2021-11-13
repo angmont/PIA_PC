f12280fc596463120cd0a9abc7501853c8b3b9e5
angmont <angela.montoyaldp@uanl.edu.mx>
2021-11-12 20:25:58 -0600

    Update dns.ps1

--- dns.ps1
+++ dns.ps1
@@ -8,26 +8,23 @@ Solo es necesario ejecutar el script, lo demás lo pide por medio de Read-Host
 $ErrorActionPreference = "SilentlyContinue"
 $op = Read-Host -prompt "¿Qué opción desea realizar?`n[1] Consulta DNS`n[2] Ver Caché`n[3] Borrar Caché`n[4] Salir`n"
 $Path = Get-Location
-try{
-	switch($op){
-    	1{ 
-        	$dom = Read-Host -prompt "Inserte el dominio"
-            	Resolve-DnsName $dom
+switch($op){
+    1{ 
+        $dom = Read-Host -prompt "Inserte el dominio"
+        Resolve-DnsName $dom
 
-    	}2{
+    }2{
 
 		Set-Content -Value (Get-DnsClientCache) -Path $Path"\Informacion_Cache.txt"
 		Write-Host "Se almacenó en el archivo Informacion_Cache.txt en la siguiente ruta:"
 		Write-Host $Path
 
-    	}3{
+    }3{
 		Clear-DnsClientCache
 	}4{
-        	exit	
+        exit	
 	}default{
-        	Write-Host "Ingreso un valor inválido"
+        Write-Host "Ingreso un valor inválido"
 	}
 	}
-}catch {
-	Write-Host "Exception"
-}
+
